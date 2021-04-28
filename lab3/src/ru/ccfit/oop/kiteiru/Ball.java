5e1528a66197619c952267a76331b7521937f9d6
kiteiru <v.pronina@g.nsu.ru>
2021-04-28 19:21:16 +0700

    new

---
+++ lab3/src/ru/ccfit/oop/kiteiru/Ball.java
@@ -0,0 +1,88 @@
+package ru.ccfit.oop.kiteiru;
+
+import javax.imageio.ImageIO;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+
+public class Ball {
+    public static final int SIZE = 45;
+
+    private int x, y;
+    private int xVel, yVel; //coordinate velocity
+    private int speed = 5;
+
+
+    private BufferedImage ballImage;
+
+    public Ball () {
+        reset();
+    }
+
+    private void reset() {
+        //initial position
+        x = (Game.WIDTH / 2) - (SIZE / 2);
+        y = (Game.HEIGHT / 2) - (SIZE / 2);
+
+        //initial velocity
+        xVel = Game.sign(Math.random() * 2.0 - 1);
+        yVel = Game.sign(Math.random() * 2.0 - 1);
+    }
+
+    public void changeXDir() { //change x direction
+        xVel *= -1;
+    }
+
+    public void changeYDir() { //change y direction
+        yVel *= -1;
+    }
+
+    public void draw(Graphics g) throws IOException {
+
+        try {
+            ballImage = ImageIO.read(getClass().getResourceAsStream("/graphics/sakura.png"));
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+        g.drawImage(ballImage, x, y, SIZE, SIZE, null);
+        //g.setColor(Color.white);
+        //g.fillRect(x, y, SIZE, SIZE);
+    }
+
+    public void update(Paddle paddle1, Paddle paddle2) {
+        //update movement of ball
+        x += xVel * speed;
+        y += yVel * speed;
+
+        //collisions
+        if ((y + SIZE) >= Game.HEIGHT || y <= 0) {
+            changeYDir();
+            //with walls
+            if ((x + SIZE) >= Game.WIDTH) {
+                paddle1.addPoint();
+                reset();
+            }
+            if (x <= 0) {
+                paddle2.addPoint();
+                reset();
+            }
+
+        }
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    /*public void checkScoreOnWin(int score) {
+        if (score == )
+    }*/
+}
---
+++ lab3/src/ru/ccfit/oop/kiteiru/Window.java
@@ -0,0 +1,18 @@
+package ru.ccfit.oop.kiteiru;
+
+import javax.swing.*;
+
+public class Window {
+    public Window(String title, Game game) {
+        JFrame frame = new JFrame(title);
+
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setResizable(false);
+        frame.add(game);
+        frame.pack();
+        frame.setLocationRelativeTo(null); //game appears centered
+        frame.setVisible(true);
+
+        game.start();
+    }
+}
